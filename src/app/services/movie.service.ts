3d0f1739cad482ea001a609890a32a4837951e68
pierre gosselin <pierre.gosselin@orange.fr>
2019-08-05 15:27:49 +0200

    Affiche de film

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,17 +8,22 @@ import { Injectable } from '@angular/core';
 export class MovieService {
   apiUrl: string = 'https://api.themoviedb.org/3';
   apiKey: string = '612ebf63e580831559365d1bc93af503';
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   /**
    * Récupère tous les films populaires
    * https://api.themoviedb.org/3/movie/popular?api_key=612ebf63e580831559365d1bc93af503&language=fr-FR&page=1
    */
-  getPopularMovies(){}
+  getPopularMovies(): Promise<any> {
+    // Aprés avoir reçu, on ne récupère que la clé results avec la liste des films
+    return this.http.get(this.apiUrl + '/movie/popular?api_key=' + this.apiKey + '&language=fr-FR&page=1').toPromise().then(response => response.results);
+  }
 
   /**
    * Récupère les détails d'un film
    */
-  getMovie(id: number){}
+  getMovie(id: number){
+
+  }
 
 }
