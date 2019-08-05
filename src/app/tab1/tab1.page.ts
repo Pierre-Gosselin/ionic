cf913ac87164c37f6c29aef2ec8418ba423638d7
pierre gosselin <pierre.gosselin@orange.fr>
2019-08-05 13:49:09 +0200

    utilisation de Camera

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers: [Camera]
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  picture: string;
+  pictures: string[] = [];
 
-  constructor() {}
+  constructor(private camera: Camera,
+    private alertController: AlertController) {}
 
+  ngOnInit(){
+  }
+
+  takePicture(){
+    const options: CameraOptions = {
+      targetWidth: 320,
+      targetHeight: 140,
+      saveToPhotoAlbum: true,
+      destinationType: this.camera.DestinationType.NATIVE_URI
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+      // On créée une modale et une fois qu'elle est prete, on l'affiche
+      // 'data:image/jpeg;base64,' + 
+      this.picture = imageData;
+      this.pictures.push(this.picture);
+    });
+  }
+  getPicture(){
+
+  }
 }
